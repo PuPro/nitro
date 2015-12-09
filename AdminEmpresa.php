c46e069ff79563f97187848ed398b46853d8390c
solangeCampos <solange.camposcampos@gmail.com>
2015-12-09 15:48:19 -0300

    primer ingreso
    ingresar

--- AdminEmpresa.php
+++ AdminEmpresa.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+f<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -7,12 +7,7 @@ and open the template in the editor.
 <html>
     <head>
 <meta charset="UTF-8">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css">
-<link rel="stylesheet" href="bootstrap/css/LargoFormControl.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-<script src="bootstrap/css/LargoFormControl.css"></script>
+<?php include 'application/view/head.php'; ?>
 <style type="text/css">
 	.bs-example{
     	margin: 20px;
---
+++ application/views/head.php
@@ -0,0 +1,3 @@
+<script src="Jquery/jquery-1.11.3.min.css"></script>
+<script src="js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
